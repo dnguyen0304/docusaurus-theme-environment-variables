ab8933bab5cd351c07d3c2567dede4da5858b281
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-31 16:09:10 -0700

    Add String to package.

---
+++ src/theme/docupotamus-environment-variables/package/index.tsx
@@ -0,0 +1,9 @@
+// Warning: Only symbols exported from this package/ directory are considered
+// public. Other symbols are considered private and _unsafe_ to be imported.
+// Their APIs are unstable and breaking changes might occur.
+
+import String from '../theme/CodeBlock/Content/String';
+
+export {
+    String,
+};
