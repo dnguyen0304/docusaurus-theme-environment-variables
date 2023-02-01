9005f894a2055d9875ed6d636c856cae9593ade1
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-01 14:28:51 -0700

    Add setEntries useEffect for WorkbenchTab component.

--- src/theme/docupotamus-environment-variables/components/WorkbenchTab/index.tsx
+++ src/theme/docupotamus-environment-variables/components/WorkbenchTab/index.tsx
@@ -1,20 +1,20 @@
 import Box from '@mui/material/Box';
 // import TextField from '@mui/material/TextField';
 import * as React from 'react';
-// import { DATA_ATTRIBUTE, TARGET_CLASS_NAME } from '../PartitionedLineTokens';
+import { DATA_ATTRIBUTE, TARGET_CLASS_NAME } from '../PartitionedLineTokens';
 // import styles from './styles.module.css';
 
 // const KEY_PREFIX = 'environmentVariable';
 
-// // TODO(dnguyen0304): Support defaultValue property.
-// // TODO(dnguyen0304): Support isRequired property.
-// interface Entry {
-//     readonly key: string;
-//     readonly element: Element;
-// };
+// TODO(dnguyen0304): Support defaultValue property.
+// TODO(dnguyen0304): Support isRequired property.
+interface Entry {
+    readonly key: string;
+    readonly element: HTMLElement;
+};
 
 export default function WorkbenchTab(): JSX.Element {
-    //     const [entries, setEntries] = React.useState<Entry[]>([]);
+    const [entries, setEntries] = React.useState<Entry[]>([]);
 
     //     const handleBlur = (selector: string) => {
     //         document.querySelector(selector)!.classList.remove('highlight');
@@ -42,23 +42,23 @@ export default function WorkbenchTab(): JSX.Element {
     //         document.querySelector(selector)!.innerHTML = event.target.value + ' ';
     //     };
 
-    //     React.useEffect(() => {
-    //         const newEntries: Entry[] = [];
-    //         document.querySelectorAll(`.${TARGET_CLASS_NAME}`).forEach(element => {
-    //             if (!(element instanceof HTMLElement)) {
-    //                 return;
-    //             }
-    //             const key = element.dataset[DATA_ATTRIBUTE];
-    //             if (!key) {
-    //                 return;
-    //             }
-    //             newEntries.push({
-    //                 key,
-    //                 element,
-    //             });
-    //         });
-    //         setEntries(newEntries);
-    //     }, []);
+    React.useEffect(() => {
+        const newEntries: Entry[] = [];
+        document.querySelectorAll(`.${TARGET_CLASS_NAME}`).forEach(element => {
+            if (!(element instanceof HTMLElement)) {
+                return;
+            }
+            const key = element.dataset[DATA_ATTRIBUTE];
+            if (!key) {
+                return;
+            }
+            newEntries.push({
+                key,
+                element,
+            });
+        });
+        setEntries(newEntries);
+    }, []);
 
     return (
         <Box>
