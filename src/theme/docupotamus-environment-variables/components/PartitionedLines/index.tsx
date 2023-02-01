7ee7be7f1bafc53b3ad874c1a2e0f4731f7e5309
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-01 09:46:19 -0700

    Add getPartitionedIndices for PartitionedLines component.

--- src/theme/docupotamus-environment-variables/components/PartitionedLines/index.tsx
+++ src/theme/docupotamus-environment-variables/components/PartitionedLines/index.tsx
@@ -3,6 +3,28 @@ import type { Props as LineProps } from '@theme/CodeBlock/Line';
 import Line from '@theme/CodeBlock/Line';
 import * as React from 'react';
 
+const REGEX = /\{\{\s\S+\s\}\}/g;
+
+const getPartitionIndices = (text: string): number[] => {
+    const matches = [
+        // See: https://github.com/microsoft/TypeScript/issues/36788
+        ...text.matchAll(REGEX) as IterableIterator<RegExpExecArray>,
+    ];
+    const indices: number[] = [];
+    // if (matches[0].index !== 0) {
+    //     // if (matches[0]?.index !== 0) {
+    //     indices.push(0);
+    // }
+    matches.forEach(match => {
+        indices.push(match.index);
+        indices.push(match.index + match[0].length);
+    });
+    // if (matches[matches.length - 1] !== text.length) {
+    //     indices.push(text.length);
+    // }
+    return indices;
+};
+
 // TODO(dnguyen0304): Investigate if importing from prism-react-renderer is
 //   possible.
 interface PrismToken {
@@ -28,6 +50,8 @@ export default function PartitionedLines(
         getTokenProps,
     }: Props,
 ): JSX.Element {
+    // console.log(getPartitionIndices(code));
+
     const chunks: JSX.Element[] = [];
 
     tokens.forEach((line, i) => {
