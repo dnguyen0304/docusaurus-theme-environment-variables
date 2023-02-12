756a79ad31169d7fa1ff42551b47c682159959f7
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-11 19:32:41 -0700

    Fix typo for docusaurus-theme-environment-variables ambient module.

--- src/docusaurus-theme-environment-variables.d.ts
+++ src/docusaurus-theme-environment-variables.d.ts
@@ -1,8 +1,8 @@
-declare module '@docupotamus/docusaurus-theme-command-menu' {
+declare module '@docupotamus/docusaurus-theme-environment-variables' {
     interface PluginOptions { }
     interface EnvironmentVariablesThemeConfig { }
 }
 
 declare module '@docusaurus/theme-environment-variables' {
-    export * from '@docupotamus/docusaurus-theme-command-menu';
+    export * from '@docupotamus/docusaurus-theme-environment-variables';
 }
